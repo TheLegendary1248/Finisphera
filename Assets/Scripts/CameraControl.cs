c48d9009a7e2307b1cf78706ff0bd7d60fa4a75d
TheLegendary1248 <1248alex@gmail.com>
2023-05-09 05:40:36 -0400

    Camera Fixes to Ball

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float lerp = 0.05f;
+    public ObjectGravity ballRef;
+
+    private void FixedUpdate()
     {
         
     }
@@ -13,6 +15,8 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float angle = transform.eulerAngles.z;
+        Vector2 refGrav = ballRef.gravity;
+        transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(angle, Mathf.Atan2(refGrav.y, refGrav.x) * Mathf.Rad2Deg + 90f, Time.deltaTime * lerp));
     }
 }
