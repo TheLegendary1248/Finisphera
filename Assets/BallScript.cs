dd81db818d05e354c93ff2cadb6f7553a5c3aed2
TheLegendary1248 <1248alex@gmail.com>
2023-05-11 01:36:17 -0400

    Camera follows Ball

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -6,7 +6,12 @@ public class BallScript : MonoBehaviour
 {
     public static event System.Action onBounce;
     public static Vector2 spawnpoint;
-
+    public static BallScript self;
+    public ObjectGravity objGrav;
+    private void Start()
+    {
+        self = this;
+    }
     private void OnCollisionEnter2D()
     {
         onBounce?.Invoke();
--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public float lerp = 0.05f;
-    public ObjectGravity ballRef;
 
     // Update is called once per frame
     void Update()
     {
-        if(ballRef)
+        if(BallScript.self.gameObject)
         {
             float angle = transform.eulerAngles.z;
-            Vector2 refGrav = ballRef.gravity;
+            Vector2 refGrav = BallScript.self.objGrav.gravity;
             transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(angle, Mathf.Atan2(refGrav.y, refGrav.x) * Mathf.Rad2Deg + 90f, Time.deltaTime * lerp));
+            transform.position = BallScript.self.transform.position;
         }
         
     }
