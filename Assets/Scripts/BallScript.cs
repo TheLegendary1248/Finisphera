30a1af13076136b539338ed4d6308cb31d405dfc
TheLegendary1248 <1248alex@gmail.com>
2023-05-18 19:24:07 -0400

    Ball keeps all previous strokes and remembers velocity. Will be part of mechanics

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    public static Vector2 spawnpoint;
-    public static Vector2 lastStrokePoint;
+    public Vector2 spawnpoint;
+    public Vector2 lastStrokePoint;
     public static BallScript self;
     public ObjectGravity objGrav;
     public Rigidbody2D rb;
     public GameObject[] disableGOList;
+    public List<BallCheckpoints> checkPoints = new List<BallCheckpoints>();
     private void Start()
     {
         self = this;
+        AddCheckpoint();
     }
+    ///<summary>Shorthand for adding a checkpoint</summary>
+    void AddCheckpoint() { checkPoints.Add(new BallCheckpoints(rb.velocity, transform.position)); Debug.Log("Added cp"); }
     private void Awake()
     {
         GameManager.onSwing += SetLastStrokePoint;
@@ -22,7 +26,7 @@ public class BallScript : MonoBehaviour
     {
         GameManager.onSwing -= SetLastStrokePoint;
     }
-    void SetLastStrokePoint(bool state) => lastStrokePoint = state ? transform.position : lastStrokePoint;
+    void SetLastStrokePoint(bool state) { if (state) AddCheckpoint(); }
     private void OnCollisionEnter2D()
     {
         GameManager.bouncesLeft -= 1;
@@ -40,10 +44,24 @@ public class BallScript : MonoBehaviour
     /// </summary>
     public void ResetToLastPoint()
     {
-        transform.position = lastStrokePoint;
+        Debug.Log("reset");
+        BallCheckpoints cp = checkPoints[^1];
+        rb.velocity = cp.velocity;
+        transform.position = cp.position;
     }
     public void FullReset()
     {
         transform.position = spawnpoint;
     }    
+}
+public struct BallCheckpoints
+{
+    public Vector2 velocity;
+    public Vector2 position;
+    //Constructor
+    public BallCheckpoints(Vector2 velocity, Vector2 position)
+    {
+        this.velocity = velocity;
+        this.position = position;
+    }
 }
